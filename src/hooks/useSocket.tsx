54042ab71385a8b78f606a6cd4bf9a284ed0ad9b
Ghalib Zaidi <ghalibzaidi115@gmail.com>
2026-03-28 01:59:31 +0500

    Refactor AuthGuard and useSocket to optimize performance and clean up code

--- src/hooks/useSocket.tsx
+++ src/hooks/useSocket.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { createContext, ReactNode, useCallback, useContext, useEffect, useRef, useState } from "react";
+import { createContext, ReactNode, useCallback, useContext, useEffect, useMemo, useRef, useState } from "react";
 import { toast } from "react-toastify";
 import { io, Socket } from "socket.io-client";
 import { getCurrentUser } from "@/lib/api/user";
@@ -113,8 +113,13 @@ export const SocketProvider = ( {children} : {children: ReactNode} ) => {
     }, [socket])
 
 
+    const value = useMemo<ISocketContext>(() => ({
+        sendMessage, socket, onlineUsers, notificationCount,
+        notifications, setNotifications, setNotificationDropdownOpen, setNotificationCount,
+    }), [sendMessage, socket, onlineUsers, notificationCount, notifications, setNotifications, setNotificationDropdownOpen, setNotificationCount]);
+
     return (
-        <SocketContext.Provider value={{ sendMessage, socket, onlineUsers, notificationCount, notifications, setNotifications, setNotificationDropdownOpen,setNotificationCount }}>
+        <SocketContext.Provider value={value}>
             { children }
         </SocketContext.Provider>
     )
